62a05cc735c1fb7fbf4074e0822e670d57ead38f
gabaeliz <elizabethg.eg12@gmail.com>
2023-03-21 11:59:04 -0600

    Correccion de puerto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { PORT } = require("./config/config.js");
 const express = require("express");
 const server = express();
-const { PORT } = require("./config/config.js");
+
 
 server.listen(process.env.PORT, () => {
     console.log(`The server is listening on port ${PORT}`);
